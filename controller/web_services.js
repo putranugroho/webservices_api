9332ce50a6179b8c9af9232216cb981b5ec1c679
Putra Nugroho <putraa.nugroho@gmail.com>
2025-01-19 10:07:23 +0700

    update implementing web services

--- controller/web_services.js
+++ controller/web_services.js
@@ -3,9 +3,10 @@ const express = require('express');
 require('dotenv').config;
 
 const url_collme = process.env.URL_COLLME
+const api_key = process.env.API_KEY
 const url_cms = process.env.CMS_URL
 
-const connect_axios = async (url, api, route, data) => {
+const connect_axios = async (url, api, route, api_key, data) => {
     try {
         let Result = ""
         console.log(`${url}${route}`);
@@ -15,7 +16,7 @@ const connect_axios = async (url, api, route, data) => {
         if (api === 'CMS') {
             headers = {
                 "Content-Type": "application/json",
-                "api-key": process.env.API_KEY
+                "api-key": api_key
             }
         } else if (api === 'CORE') {
             headers = {
@@ -59,7 +60,7 @@ const report = async (req, res) => {
     let {nohp, report_type} = req.body;
     try {
         const data_report = {nohp, report_type}
-        let request_report = await connect_axios(url_collme, 'CMS', 'collme/cis/report', data_report)
+        let request_report = await connect_axios(url_collme, 'CMS', 'collme/cis/report', api_key, data_report)
         if (request_report.code !== "000" && request_report.data === null) {
             console.log(request_report);
             res.status(200).send(request_report);
@@ -68,7 +69,7 @@ const report = async (req, res) => {
                 code: "000",
                 status: "ok",
                 message: "Success",
-                data: request.data,
+                data: request_report.data,
             }
             console.log(res_send);
             res.status(200).send(res_send);
@@ -84,7 +85,7 @@ const report_periode = async (req, res) => {
     let {nohp, tgl_awal, tgl_akhir} = req.body;
     try {
         const data_report = {nohp, tgl_awal, tgl_akhir}
-        let request_report = await connect_axios(url_collme, 'CMS', 'collme/cis/report/periode', data_report)
+        let request_report = await connect_axios(url_collme, 'CMS', 'collme/cis/report/periode', api_key, data_report)
         if (request_report.code !== "000" && request_report.data === null) {
             console.log(request_report);
             res.status(200).send(request_report);
@@ -93,7 +94,7 @@ const report_periode = async (req, res) => {
                 code: "000",
                 status: "ok",
                 message: "Success",
-                data: request.data,
+                data: request_report.data,
             }
             console.log(res_send);
             res.status(200).send(res_send);
